4b8fc8cd5aaa05bd81aa96118ce9a645961b20b2
Sai-Mithra <105640472+mitrasai@users.noreply.github.com>
2024-08-07 14:09:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+var typed = new Typed('#element', {
+    strings: [`<i>Welcome! I'm an aspiring software engineer with a strong passion for clean and efficient code. My keen eye for design allows me to understand the intitution behind problem. I am highly adaptable, always eager to learn new things and experiment them. My goal is to secure a position where I can contribute my skills and grow as a professional.</i>`],
+    typeSpeed: 20,
+  });
